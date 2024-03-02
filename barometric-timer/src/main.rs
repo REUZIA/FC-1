dbd22927afd7d5a17e5a630636db6ec901178f92
Nicolas THIERRY <nicolas.thierry.perso@gmail.com>
2024-03-02 16:47:48 +0000

    add: interrupt as backup timer

--- barometric-timer/src/main.rs
+++ barometric-timer/src/main.rs
@@ -6,17 +6,19 @@ mod rp_2040_lib;
 use core::iter::once;
 use embedded_hal::timer::CountDown;
 use embedded_hal::{digital::v2::InputPin, PwmPin};
-use fugit::{ExtU32, RateExtU32};
+use fugit::{ExtU32, MicrosDurationU32, RateExtU32, Rate};
 use micromath::F32Ext;
 use panic_halt as _;
 use rp_2040_lib::{
     entry,
     hal::{
         clocks::{init_clocks_and_plls, Clock, PeripheralClock, SystemClock, UsbClock},
-        gpio, i2c, pac,
+        pio,
+        gpio, i2c,
+        pac::{self, interrupt},
         pio::PIOExt,
         pwm, reset,
-        timer::Timer,
+        timer::{Alarm, Timer},
         usb::UsbBus,
         watchdog::Watchdog,
         Sio,
@@ -24,15 +26,18 @@ use rp_2040_lib::{
     Pins, XOSC_CRYSTAL_FREQ,
 };
 
-//Addressable LED imports
+// Addressable LED imports
 use smart_leds::{brightness, SmartLedsWrite, RGB8};
 use ws2812_pio::Ws2812;
-//BMP280 imports
+// BMP280 imports
 use bmp280_rs::{Config, I2CAddress, BMP280};
 // USB imports
 use core::fmt::Write;
 use usb_device::{class_prelude::*, prelude::*};
 use usbd_serial::SerialPort;
+// IRQ imports
+use core::cell::RefCell;
+use critical_section::Mutex;
 
 use embedded_alloc::Heap;
 #[global_allocator]
@@ -46,16 +51,15 @@ use littlefs2::{
     driver::Storage,
     fs::{FileType, Filesystem},
     io::SeekFrom,
-    path::{Path, PathBuf},
+    path::Path,
 };
 use rp2040_flash::flash;
 const XIP_BASE: u32 = 0x10000000;
 const PAGE_SIZE: u32 = 256;
 const SECTOR_SIZE: u32 = 4 * 1024;
 const FLASH_SIZE: u32 = 2 * 1024 * 1024;
-const PROGRAM_SIZE: u32 = SECTOR_SIZE * 60; // 245KB
+const PROGRAM_SIZE: u32 = SECTOR_SIZE * 64; // 262KB
 
-const ALTITUDE_THRESHOLD: f32 = 15.0_f32;
 
 struct RpRom;
 impl Storage for RpRom {
@@ -155,7 +159,11 @@ fn main() -> ! {
     )
     .ok()
     .unwrap();
-    let timer = Timer::new(pac.TIMER, &mut pac.RESETS, &clocks);
+    let mut timer = Timer::new(pac.TIMER, &mut pac.RESETS, &clocks);
+
+    critical_section::with(|cs| {
+        TIMER_AND_CLOCK.borrow(cs).replace(Some((timer, clocks.peripheral_clock.freq())));
+    });
 
     let sio = Sio::new(pac.SIO);
     let pins = Pins::new(
@@ -201,11 +209,13 @@ fn main() -> ! {
         pins.gp3.reconfigure(),
         pins.neopixel,
         pins.gp10,
-        timer,
+        &mut timer,
         &clocks.peripheral_clock,
         &clocks.system_clock,
         &fs,
-    )
+    );
+
+    loop {}
 }
 
 enum FrameLog {
@@ -214,12 +224,12 @@ enum FrameLog {
 }
 struct Logger<'a> {
     fs: &'a Filesystem<'a, RpRom>,
-    system_clock: &'a SystemClock,
+    timer: &'a Timer,
     run_path: String,
     baked_log_path: String,
 }
 impl<'a> Logger<'a> {
-    pub fn new(fs: &'a Filesystem<'a, RpRom>, system_clock: &'a SystemClock) -> Self {
+    pub fn new(fs: &'a Filesystem<'a, RpRom>, timer: &'a Timer) -> Self {
         let mut run_path: String = String::new();
         // Create a new dir for the current run
         fs.read_dir_and_then(&Path::from_str_with_nul("/\0"), |dirs| {
@@ -234,15 +244,37 @@ impl<'a> Logger<'a> {
             Ok(())
         })
         .unwrap();
+
         let mut log = Logger {
             fs,
-            system_clock,
+            timer,
             run_path,
             baked_log_path: String::new(),
         };
-        log.write_event_log("System initialized!");
+        log.write_event_log("LOG INIT");
         log
     }
+
+    pub fn from_current(fs: &'a Filesystem<'a, RpRom>, timer: &'a Timer) -> Self {
+        let mut run_path: String = String::new();
+        fs.read_dir_and_then(&Path::from_str_with_nul("/\0"), |dirs| {
+            let dir_count = dirs.count() - 2; // -2 because of /. and /..
+            if dir_count > 9999 {
+                panic!("Too many runs, please clear the storage");
+            }
+            write!(&mut run_path, "run_{}", dir_count-1).unwrap();
+            Ok(())
+        })
+        .unwrap();
+        let mut log = Logger {
+            fs,
+            timer,
+            run_path,
+            baked_log_path: String::new(),
+        };
+        log.write_event_log("LOG ATTACHED");
+        log
+    } 
 
     pub fn write_event_log(&mut self, log: &str) {
         // Baked log path for farter execution
@@ -257,17 +289,111 @@ impl<'a> Logger<'a> {
                 |options| options.read(true).append(true).create(true),
                 &Path::from_str_with_nul(self.baked_log_path.as_str()),
                 |file| {
-                    let mut temp_str = String::new();
-                    temp_str.push((FrameLog::Event as u8) as char);
-                    temp_str.push('|');
-                    temp_str.push_str(log);
-                    temp_str.push('\0');
-                    file.write(temp_str.as_bytes())?;
+                    let mut temp_str = Vec::<u8>::new();
+                    temp_str.push(FrameLog::Event as u8);
+                    temp_str.push(b'|');
+                    temp_str.extend_from_slice(&self.timer.get_counter().ticks().to_le_bytes());
+                    temp_str.push(b'|');
+                    temp_str.extend_from_slice(log.as_bytes());
+                    temp_str.push(b'\0');
+                    file.write(&temp_str)?;
                     Ok(())
                 },
             )
             .unwrap();
     }
+
+    pub fn write_bmp_log(&mut self, temp: i32, pressure: i32, altitude: f32) {
+        // Baked log path for farter execution
+        if self.baked_log_path.is_empty() {
+            let mut temp_path: String = String::new();
+            write!(&mut temp_path, "{}/log\0", self.run_path).unwrap();
+            self.baked_log_path = temp_path;
+        }
+
+        self.fs
+            .open_file_with_options_and_then(
+                |options| options.read(true).append(true).create(true),
+                &Path::from_str_with_nul(self.baked_log_path.as_str()),
+                |file| {
+                    let mut temp_str = Vec::<u8>::new();
+                    temp_str.push(FrameLog::BMP as u8);
+                    temp_str.push(b'|');
+                    temp_str.extend_from_slice(&self.timer.get_counter().ticks().to_le_bytes());
+                    temp_str.push(b'|');
+                    temp_str.extend_from_slice(&temp.to_le_bytes());
+                    temp_str.extend_from_slice(&pressure.to_le_bytes());
+                    temp_str.push(b'\0');
+                    file.write(&temp_str)?;
+                    Ok(())
+                },
+            )
+            .unwrap();
+    }
+}
+
+type TimerAndClock = (Timer, Rate<u32, 1, 1>);
+static TIMER_AND_CLOCK: Mutex<RefCell<Option<TimerAndClock>>> = Mutex::new(RefCell::new(None));
+// Interrupt Triggered by the Alarm1 of TIMER
+#[interrupt]
+fn TIMER_IRQ_1() {
+    let mut timerclock: Option<(Timer, Rate<u32, 1, 1>)> = None;
+    critical_section::with(|cs| {
+        // Temporarily take our LED_AND_ALARM
+        timerclock = TIMER_AND_CLOCK.borrow(cs).take();
+    });
+
+    if let Some((timer, peripheral_clock_freq)) = timerclock {
+        let mut pac = unsafe { pac::Peripherals::steal() };
+
+        let sio = Sio::new(pac.SIO);
+        let pins = Pins::new(
+            pac.IO_BANK0,
+            pac.PADS_BANK0,
+            sio.gpio_bank0,
+            &mut pac.RESETS,
+        );
+
+        // Configure the addressable LED
+        let (mut pio, sm0, _, _, _) = pac.PIO0.split(&mut pac.RESETS);
+        let mut ws = Ws2812::new(
+            // The onboard NeoPixel is attached to GPIO pin #16 on the Feather RP2040.
+            pins.neopixel.into_function(),
+            &mut pio,
+            sm0,
+            peripheral_clock_freq,
+            timer.count_down(),
+        );
+
+        // Configure Filesystem
+        let mut storage = RpRom;
+
+        let mut alloc = Filesystem::allocate();
+        if !Filesystem::is_mountable(&mut storage) {
+            Filesystem::format(&mut storage).unwrap();
+        }
+        let fs = Filesystem::mount(&mut alloc, &mut storage).unwrap();
+        let mut logger = Logger::from_current(&fs, &timer);
+
+        ws.write(brightness(once(RGB8::new(255, 255, 255)), 100))
+            .unwrap();
+
+        let mut pwm_slices = pwm::Slices::new(pac.PWM, &mut pac.RESETS);
+        let pwm = &mut pwm_slices.pwm5;
+        pwm.clr_ph_correct(); // Not use phase correct PWM Datasheet 4.5.2.1
+                              // Require 20ms period period(s) = ((TOP + 1) * (DIV_INT + DIV_FRAC / 16)) / fclk_MHz
+        let pwm_params = compute_pwm_params(peripheral_clock_freq.to_Hz(), 50);
+        pwm.set_top(pwm_params.top);
+        pwm.set_div_int(pwm_params.div_int);
+        pwm.set_div_frac(pwm_params.div_frac);
+        pwm.enable();
+        let servo_pwm = &mut pwm.channel_a;
+        servo_pwm.output_to(pins.gp10);
+        servo_pwm.set_duty(get_count_from_us(1000, &pwm_params)); // Open Servo
+
+        logger.write_event_log("FORCED DEPLOYEMENT");
+    }
+    // WARNING: NO JUMP BACK !! (don't know why ?)
 }
 
 fn normal_mode<'a>(
@@ -280,11 +406,17 @@ fn normal_mode<'a>(
     scl_pin: gpio::Pin<gpio::bank0::Gpio3, gpio::FunctionI2c, gpio::PullUp>,
     neopixel: gpio::Pin<gpio::bank0::Gpio16, gpio::FunctionNull, gpio::PullDown>,
     servo_pin: gpio::Pin<gpio::bank0::Gpio10, gpio::FunctionNull, gpio::PullDown>,
-    timer: Timer,
+    timer: &'a mut Timer,
     peripheral_clock: &PeripheralClock,
-    system_clock: &'a SystemClock,
+    system_clock: &SystemClock,
     file_system: &'a Filesystem<'a, RpRom>,
-) -> ! {
+) {
+    const BUFFER_MAX_LEN: usize = 32;
+    const MAX_DELAY: MicrosDurationU32 = MicrosDurationU32::minutes(2);
+    const ALTITUDE_THRESHOLD: f32 = 0.0_f32;
+
+    let mut timer_alarm = timer.alarm_1().unwrap();
+
     // Configure the addressable LED
     let (mut pio, sm0, _, _, _) = pac_pio0.split(pac_resets);
     let mut ws = Ws2812::new(
@@ -337,7 +469,7 @@ fn normal_mode<'a>(
     .unwrap();
 
     // Creating logger handler
-    let mut logger = Logger::new(file_system, system_clock);
+    let mut logger = Logger::new(file_system, timer);
 
     // FORCE DEPLOYEMENT
     ws.write(brightness(once(RGB8::new(0, 255, 0)), 100))
@@ -347,7 +479,7 @@ fn normal_mode<'a>(
     let _ = nb::block!(delay.wait());
     if modeselect_pin.is_low().unwrap() {
         servo_pwm.set_duty(servo_open);
-        logger.write_event_log("Force deployement!");
+        logger.write_event_log("FORCED DEPLOYEMENT");
         loop {
             // Turn off the LED (black)
             ws.write(brightness(once(RGB8::new(0, 0, 0)), 0)).unwrap();
@@ -362,22 +494,30 @@ fn normal_mode<'a>(
     }
 
     // STANDBY / WAIT FOR LAUNCH
-    logger.write_event_log("Waiting for launch...");
+    logger.write_event_log("WAITING FOR LAUNCH...");
     ws.write(brightness(once(RGB8::new(255, 0, 0)), 100))
         .unwrap();
-    let mut buffer_index: usize = 0;
-    let mut circ_buffer = [0u8; PAGE_SIZE as usize];
+    let mut buffer_head_index: usize = 0;
+    let mut buffer_len: usize = 0;
+    let mut circ_buffer = [0u32; BUFFER_MAX_LEN * 3];
     let mut sea_level_pressure = 0;
     loop {
-        let _ = bmp.read_temperature(&mut i2c).unwrap();
+        let temp = bmp.read_temperature(&mut i2c).unwrap();
         let pressure = bmp.read_pressure(&mut i2c).unwrap();
         if sea_level_pressure == 0 {
             sea_level_pressure = pressure;
         }
-        circ_buffer[buffer_index..buffer_index + 4].copy_from_slice(&pressure.to_be_bytes());
-        buffer_index = (buffer_index + 4) % PAGE_SIZE as usize;
         let altitude: f32 =
             44330.0 * (1.0 - ((pressure as f32) / (sea_level_pressure as f32)).powf(0.1903));
+
+        circ_buffer[buffer_head_index] = temp as u32;
+        circ_buffer[buffer_head_index + 1] = pressure as u32;
+        circ_buffer[buffer_head_index + 2] = altitude.to_bits();
+
+        buffer_head_index = (buffer_head_index + 3) % BUFFER_MAX_LEN*3 as usize;
+        if buffer_len < BUFFER_MAX_LEN as usize {
+            buffer_len += 3;
+        };
 
         if altitude > ALTITUDE_THRESHOLD {
             break;
@@ -391,16 +531,33 @@ fn normal_mode<'a>(
                 .unwrap();
         }
     }
-
-    logger.write_event_log("Launch detected!");
+    logger.write_event_log("LAUNCH DETECTED");
     ws.write(brightness(once(RGB8::new(0, 0, 255)), 100))
         .unwrap();
 
+    logger.write_event_log("SETUP BACKUP TIMER");
+    critical_section::with(|_| { 
+        timer_alarm.clear_interrupt();
+        timer_alarm.schedule(MAX_DELAY).unwrap();
+        timer_alarm.enable_interrupt();
+
+        // Unmask the timer0 IRQ so that it will generate an interrupt
+        unsafe {
+            pac::NVIC::unmask(pac::Interrupt::TIMER_IRQ_1);
+        }
+    });
+
+    logger.write_event_log("SAVE CIRCULAR BUFFER");
+    for i in ((buffer_head_index - buffer_len)..buffer_head_index).step_by(3) {
+        let i = i % BUFFER_MAX_LEN;
+        logger.write_bmp_log(circ_buffer[i] as i32, circ_buffer[i + 1] as i32, f32::from_bits(circ_buffer[i + 2]));
+    }
+
+    logger.write_event_log("START MAIN LOOP");
     loop {}
 }
 
 const SERIAL_BUFFER_SIZE: usize = 64;
-
 fn cli_mode(
     pac_pio0: pac::PIO0,
     pac_pwm: pac::PWM,
@@ -553,7 +710,8 @@ fn cli_mode(
                             }
 
                             for bytes_path in args.iter().skip(1) {
-                                let mut string_path = String::from_utf8(bytes_path.as_bytes().to_vec()).unwrap();
+                                let mut string_path =
+                                    String::from_utf8(bytes_path.as_bytes().to_vec()).unwrap();
                                 string_path.push('\0');
 
                                 match file_system
@@ -568,7 +726,9 @@ fn cli_mode(
                                     Ok(metadata) => match metadata.file_type() {
                                         FileType::File => {
                                             file_system
-                                                .remove(&Path::from_str_with_nul(string_path.as_str()))
+                                                .remove(&Path::from_str_with_nul(
+                                                    string_path.as_str(),
+                                                ))
                                                 .unwrap_or_else(|err| {
                                                     let mut text: String = String::new();
                                                     write!(&mut text, "err:{:?}\r\n", err).unwrap();
